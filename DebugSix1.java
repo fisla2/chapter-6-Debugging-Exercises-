61cd8de0515c9edf36be5c410aa9ac589eaaac6e
fisla2 <fisla1146362@mail.wcccd.edu>
2025-04-30 20:49:57 -0400

    Create DebugSix1.java

---
+++ DebugSix1.java
@@ -0,0 +1,9 @@
+public class FixDebugSix1
+{
+   public static void main(String[] args)
+   {
+      int[] numbers = {1, 2, 3, 4, 5};
+      for(int i = 0; i < numbers.length; i++) // Changed <= to <
+         System.out.println("Number: " + numbers[i]); // Fixed array access syntax
+   }
+}
