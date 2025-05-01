362adf0638f5749df94b6545e22996dcc009a716
fisla2 <fisla1146362@mail.wcccd.edu>
2025-04-30 20:50:55 -0400

    Create DebugSix2.java

---
+++ DebugSix2.java
@@ -0,0 +1,9 @@
+public class FixDebugSix2
+{
+   public static void main(String[] args)
+   {
+      String[] animals = {"cat", "dog", "bird"}; // Added semicolon
+      for(int i = 0; i < animals.length; i++)
+         System.out.println(animals[i]); // Corrected variable name
+   }
+}
